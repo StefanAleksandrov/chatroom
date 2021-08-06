ff0ec7194e7c59bc721c3bf29f71ce957f96c807
Stefan Aleksandrov <arh.inj.alexandrov@abv.bg>
2021-08-06 19:46:18 +0300

    Added Login and Logout functionality

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -12,14 +12,17 @@ const authService = require('../services/authService');
 router.post('/register', (req, res, next) => {
     const { username, password, repeatPassword } = req.body;
 
+    //Check if the request body has all the required information
+    if (!username || !password || !repeatPassword) return void res.status(401).json({message: "Provide all required information!"}).send();
+    //Check if the passwords match each other
     if (password != repeatPassword) return void res.status(401).json({message: "Passwords should match"}).send();
 
-    authService.getUserByUsername(username)
+    authService.getUserByUsername(username.toLowerCase())
         .then(user => {
             //Check if user with this username already exists and if so return error
             if (user) throw ({message: "User already exists", status: 409});
             //Else attempt to create new user
-            else return authService.register(username, password, repeatPassword);
+            else return authService.register(username.toLowerCase(), password);
         })
         .then(data => res.status(201).json({ message: "success", user: data }).send())
         .catch(next);
@@ -27,9 +30,27 @@ router.post('/register', (req, res, next) => {
 
 router.post('/login', (req, res, next) => {
     const { username, password } = req.body;
+
+    //Check if the request body has all the required information
+    if (!username || !password) return void res.status(401).json({message: "Provide all required information!"}).send();
+
     authService.login(username, password)
-        .then(data => res.status(201).json({ message: "success", user: data }).send())
+        .then(data => {
+            res.status(201)
+                .cookie(COOKIE_NAME, data[0], { httpOnly: true })
+                .cookie('username', data[1].username)
+                .json({ message: "success", user: data[1] })
+                .send();
+        })
         .catch(next);
+});
+
+router.post('/logout', (req, res, next) => {
+    res.status(200)
+        .cookie(COOKIE_NAME, data[0], { httpOnly: true })
+        .cookie('username', data[1].username)
+        .json({ message: "success" })
+        .send();
 });
 
 module.exports = router;
--- server/services/authService.js
+++ server/services/authService.js
@@ -4,13 +4,27 @@ const jwt = require('jsonwebtoken');
 const { SECRET } = require("../config/config");
 
 const auth = {
-    register(username, password, repeatPassword) {
+    register(username, password) {
         let newUser = new User({ username, password });
         return newUser.save();
     },
 
-    login(username, password) {
+    login(username, password, next) {
+        let foundUser = {};
+        
+        return User.findOne({username})
+            .then(user => {
+                if (!user) throw ({ status: 404, message: "Incorrect username or password." });
+                
+                foundUser = user;
+                return bcrypt.compare(password, user.password);
+            })
+            .then(areEqual => {
+                if (!areEqual) throw ({ status: 404, message: "Incorrect username or password." });
 
+                return [jwt.sign({_id: foundUser._id, username: foundUser.username, role: foundUser.role}, SECRET), foundUser];
+            })
+            .catch(next);
     },
 
     getUserByUsername(username) {
