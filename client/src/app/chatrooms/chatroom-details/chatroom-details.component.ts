337ba4442c4d3adfec549a0db14d7a307c6edcfe
Stefan Aleksandrov <arh.inj.alexandrov@abv.bg>
2021-08-18 00:25:43 +0300

    Init chatroom list and details components

---
+++ client/src/app/chatrooms/chatroom-details/chatroom-details.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-chatroom-details',
+  templateUrl: './chatroom-details.component.html',
+  styleUrls: ['./chatroom-details.component.scss']
+})
+export class ChatroomDetailsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
