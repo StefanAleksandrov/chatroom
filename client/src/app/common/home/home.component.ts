a238f4e88e988be1846c2a06da65febcd70ea6fa
Stefan Aleksandrov <arh.inj.alexandrov@abv.bg>
2021-08-18 00:24:10 +0300

    Update home page

--- client/src/app/common/home/home.component.ts
+++ client/src/app/common/home/home.component.ts
@@ -1,15 +1,24 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent {
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(
+    private router: Router
+  ) {
+    
   }
 
+  goToRegister() {
+    return this.router.navigate(['/register']);
+  }
+
+  goToLogin() {
+    return this.router.navigate(['/login']);
+  }
 }
