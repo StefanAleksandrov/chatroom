be8dd282be7b29c7f8bdca180e4cf77fdd119e38
Stefan Aleksandrov <arh.inj.alexandrov@abv.bg>
2021-08-18 22:48:26 +0300

    Import reactive forms module

--- client/src/app/user/user.module.ts
+++ client/src/app/user/user.module.ts
@@ -4,6 +4,7 @@ import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
 import { ProfileComponent } from './profile/profile.component';
 import { UserRoutingModule } from './user-routing.module';
+import { ReactiveFormsModule } from '@angular/forms';
 
 
 
@@ -15,7 +16,8 @@ import { UserRoutingModule } from './user-routing.module';
   ],
   imports: [
     CommonModule,
-    UserRoutingModule
+    UserRoutingModule,
+    ReactiveFormsModule
   ]
 })
 export class UserModule { }
