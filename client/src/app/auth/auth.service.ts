344f6323edadeb6529b33e00c9a1b286ea108193
Stefan Aleksandrov <arh.inj.alexandrov@abv.bg>
2021-08-22 09:11:46 +0300

    Update auth.service.ts

--- client/src/app/auth/auth.service.ts
+++ client/src/app/auth/auth.service.ts
@@ -1,32 +1,55 @@
-import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Inject, Injectable } from '@angular/core';
 
 // Interfaces
 import { IUser } from '../shared/interfaces';
+
+import { environment } from '../../environments/environment';
+import { LocalStorage } from '../shared/injection-tokens';
+const API_URL = environment.apiURL;
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
   user: IUser | undefined;
+  token: string | undefined;
 
-  constructor() {
-    setTimeout(() => {
-      this.user = {
-        email: "string",
-        username: "string"
-      }
-    }, 1500);
+  constructor(
+    private http: HttpClient,
+    @Inject(LocalStorage) private localStorage: Window['localStorage']
+  ) {
+    // When created check if we have <USER> in the localStorage and if so, set the logged in user
+    const loggedUser = this.localStorage.getItem('<USER>');
+    const token = this.localStorage.getItem('<TOKEN>');
+
+    if (loggedUser) {
+      this.user = JSON.parse(loggedUser);
+    }
+
+    if (token) {
+      this.token = token;
+    }
   }
 
-  login(email: string, password: string) :void {
-    this.user = {
-      email,
-      username: "John Doe"
-    }
+  login(email: string, password: string) {
+    return this.http.post<any>(`${API_URL}/auth/login`, {email, password}, { withCredentials: true });
+  }
+
+  register(email: string, password: string, repeatPassword: string) {
+    return this.http.post<any>(`${API_URL}/auth/register`, {email, password, repeatPassword});
   }
 
   logout() :void {
     this.user = undefined;
+    this.localStorage.removeItem('<USER>');
+    this.localStorage.removeItem('<TOKEN>');
+  }
+
+  setUser(user: IUser, token: string): void {
+    this.user = user;
+    this.localStorage.setItem('<USER>', JSON.stringify(user));
+    this.localStorage.setItem('<TOKEN>', token);
   }
 
   getUser() :IUser | undefined {
