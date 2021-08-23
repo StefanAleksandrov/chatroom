028c99730295e1c46314d4dddb8c036ff8ffd9a3
Stefan Aleksandrov <arh.inj.alexandrov@abv.bg>
2021-08-24 00:59:38 +0300

    Chatroom list component displays only user's chatrooms

--- client/src/app/chatrooms/chatroom-list/chatroom-list.component.ts
+++ client/src/app/chatrooms/chatroom-list/chatroom-list.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { AuthService } from 'src/app/auth/auth.service';
 import { IChatroom } from 'src/app/shared/interfaces/chatroom';
+import { ChatroomService } from '../chatroom.service';
 
 @Component({
   selector: 'app-chatroom-list',
@@ -13,19 +15,15 @@ export class ChatroomListComponent implements OnInit {
 
   constructor(
     private router: Router,
+    private chatroomService: ChatroomService,
+    private authService: AuthService
   ) {
-    this.chatrooms = [
-      {name: "test1", description: "desc test", image: 'https://media.wired.com/photos/5bb6accf0abf932caf294b18/125:94/w_2375,h_1786,c_limit/waves-730260985.jpg', owner_id: ["asd"], members: ["asd"], messages: [], created_at: new Date().toString(), _id: "asdasda124531"},
-      {name: "test2", description: "desc test", image: 'https://res.cloudinary.com/dk-find-out/image/upload/q_80,w_1920,f_auto/A-Alamy-BXWK5E_vvmkuf.jpg', owner_id: ["asd"], members: ["asd"], messages: [], created_at: new Date().toString(), _id: "65a4sd21as56d"},
-      {name: "test3", description: "desc test desc test desc test desc test", image: 'https://i.hurimg.com/i/hdn/75/0x0/5dcd630a0f25441794d60a1c.jpg', owner_id: ["asd"], members: ["asd"], messages: [], created_at: new Date().toString(), _id: "a56sd456a4sd"},
-      {name: "test4", description: "desc test", image: 'https://www.collinsdictionary.com/images/full/river_377603497_1000.jpg', owner_id: ["asd"], members: ["asd"], messages: [], created_at: new Date().toString(), _id: "5a4sd654asd6"},
-      {name: "test1", description: "desc test", image: 'https://media.wired.com/photos/5bb6accf0abf932caf294b18/125:94/w_2375,h_1786,c_limit/waves-730260985.jpg', owner_id: ["asd"], members: ["asd"], messages: [], created_at: new Date().toString(), _id: "56a4sd65a1sd65"},
-    ]
-
+    this.chatrooms = undefined;
     this.activeChat = undefined;
   }
 
   ngOnInit(): void {
+    this.getMyChatrooms();
   }
 
   activateChat(id: string) {
@@ -35,5 +33,14 @@ export class ChatroomListComponent implements OnInit {
 
   createNewChatroom() {
     this.router.navigate(['chatrooms/add']);
+  }
+
+  getMyChatrooms(): void {
+    this.chatroomService.getChatrooms().subscribe(data => {
+      const user = this.authService.getUser();
+      this.chatrooms = data.chatrooms.filter((el: { members: string | string[]; }) => {
+        return el.members.includes(user!._id)
+      });
+    });
   }
 }
