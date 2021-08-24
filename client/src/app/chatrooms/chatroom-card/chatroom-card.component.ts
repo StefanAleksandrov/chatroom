76403f976eca50da529633d46be6a91164c40e90
Stefan Aleksandrov <arh.inj.alexandrov@abv.bg>
2021-08-25 01:21:44 +0300

    CLIENT Join and leave Chatroom functionality implemented

--- client/src/app/chatrooms/chatroom-card/chatroom-card.component.ts
+++ client/src/app/chatrooms/chatroom-card/chatroom-card.component.ts
@@ -1,5 +1,9 @@
 import { Component, Input, OnInit } from '@angular/core';
+import { AuthService } from 'src/app/auth/auth.service';
+import { IUser } from 'src/app/shared/interfaces';
 import { IChatroom } from 'src/app/shared/interfaces/chatroom';
+import { NotificationService } from 'src/app/shared/notification.service';
+import { ChatroomService } from '../chatroom.service';
 
 @Component({
   selector: 'app-chatroom-card',
@@ -9,7 +13,50 @@ import { IChatroom } from 'src/app/shared/interfaces/chatroom';
 export class ChatroomCardComponent implements OnInit {
   @Input() chatroom:IChatroom | undefined = undefined;
 
-  constructor() { }
+  user: IUser | undefined
 
-  ngOnInit(): void {}
+  constructor(
+    private authService: AuthService,
+    private chatroomService: ChatroomService,
+    private notificationService: NotificationService
+  ) {
+    this.user = undefined;
+  }
+
+  ngOnInit(): void {
+    this.user = this.authService.getUser();
+  }
+
+  prepareData():object{
+    return {
+      user_id: this.user!._id,
+      chatroom_id: this.chatroom!._id
+    }
+  }
+
+  editChatroom(){
+    console.log(this.prepareData());
+
+  }
+
+  deleteChatroom(){
+    if (confirm("Are you sure you want to delete this chatroom?")){
+      this.chatroomService.deleteChatroom(this.chatroom!._id);
+    }
+  }
+
+  joinChatroom(){
+    this.chatroomService.joinChatroom(this.prepareData()).subscribe(() => {
+      this.chatroom?.members.push(this.user!._id);
+      this.notificationService.setNotification({message: "You joined the chatroom", type: "success"})
+    });
+  }
+
+  leaveChatroom(){
+    this.chatroomService.leaveChatroom(this.prepareData()).subscribe(() => {
+      const index = Number(this.chatroom?.members.indexOf(this.user!._id));
+      this.chatroom?.members.splice(index, 1);
+      this.notificationService.setNotification({message: "You left the chatroom", type: "success"})
+    });
+  }
 }
--- client/src/app/chatrooms/chatroom.service.ts
+++ client/src/app/chatrooms/chatroom.service.ts
@@ -1,15 +1,15 @@
-import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { HttpClient, HttpHeaders, HttpResponse } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
 // Interfaces
 import { IChatroom } from '../shared/interfaces/chatroom';
+import { IUser } from '../shared/interfaces';
 
 // Services
 import { AuthService } from '../auth/auth.service';
 
 // Environment const
 import { environment } from '../../environments/environment';
-import { IUser } from '../shared/interfaces';
 const API_URL = environment.apiURL;
 
 @Injectable({
@@ -40,5 +40,17 @@ export class ChatroomService {
     const criteria = encodeURI(searchString);
     
     return this.http.get<any>(`${API_URL}/api/chatrooms/?criteria=${criteria}`);
+  }
+
+  joinChatroom(payload: object){
+    return this.http.post<HttpResponse<object>>(`${API_URL}/api/chatrooms/join`, payload);
+  }
+
+  leaveChatroom(payload: object){
+    return this.http.post<HttpResponse<object>>(`${API_URL}/api/chatrooms/leave`, payload);
+  }
+
+  deleteChatroom(chatroom_id: string){
+    return this.http.delete(`${API_URL}/api/chatrooms/delete/${chatroom_id}`);
   }
 }
