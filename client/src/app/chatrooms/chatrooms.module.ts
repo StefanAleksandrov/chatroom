c01848ca3c152126f6a4d602d3b0a8498ff282b1
Stefan Aleksandrov <arh.inj.alexandrov@abv.bg>
2021-08-27 01:17:14 +0300

    Update

--- client/src/app/chatrooms/chatrooms.module.ts
+++ client/src/app/chatrooms/chatrooms.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 // Routing module
 import { ChatroomRoutingModule } from './chatroom-routing.module';
@@ -9,16 +10,15 @@ import { ChatroomListComponent } from './chatroom-list/chatroom-list.component';
 import { ChatroomDetailsComponent } from './chatroom-details/chatroom-details.component';
 import { ChatroomCardComponent } from './chatroom-card/chatroom-card.component';
 import { ChatroomCreateComponent } from './chatroom-create/chatroom-create.component';
-import { FormsModule } from '@angular/forms';
-
-
+import { ChatroomMessageComponent } from './chatroom-message/chatroom-message.component';
 
 @NgModule({
   declarations: [
     ChatroomListComponent,
     ChatroomDetailsComponent,
     ChatroomCardComponent,
-    ChatroomCreateComponent
+    ChatroomCreateComponent,
+    ChatroomMessageComponent
   ],
   imports: [
     FormsModule,
